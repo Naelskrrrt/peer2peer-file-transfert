e80c59c875bd55d69486d7d422cbed87d87d52cf
NaelSkrrrt <ralalanael@gmail.com>
2024-05-22 15:55:29 +0300

    22/05/2024

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ root.geometry("450x560+500+200")
 root.configure(bg="#f4fdfe")
 root.resizable(False,False)
 
+def Send():
+    window=Toplevel(root)
+    window.title("Send")
+    window.geometry("450x560+500+200")
+    window.configure(bg="#f4fdfe")
+    window.resizable(False, False)
+    
+    #icon
+    image_icon1=PhotoImage(file="image/send.png")
+    window.iconphoto(False, image_icon1)
+    
+    window.mainloop()
+    
+def Receive():
+    print()
+
 #icon
 image_icon=PhotoImage(file="image/icon.png")
 root.iconphoto(False, image_icon)
@@ -21,11 +37,11 @@ Label(root, text="File Tranfert", font=(fontFamily, 20, 'bold'), bg="#f4fdfe").p
 Frame(root, width=400, height=2, bg="#f3f5f6").place(x=25, y=80)
 
 send_image=PhotoImage(file="image/send.png")
-send=Button(root, image=send_image, bg="#f4fdfe", bd=0)
+send=Button(root, image=send_image, bg="#f4fdfe", bd=0, command=Send)
 send.place(x=50, y=100)
 
 receive_image=PhotoImage(file="image/receive.png")
-receive=Button(root, image=receive_image, bg="#f4fdfe", bd=0)
+receive=Button(root, image=receive_image, bg="#f4fdfe", bd=0, command=Receive)
 receive.place(x=300, y=100)
 
 # label
@@ -33,8 +49,8 @@ Label(root, text="Send", font=(fontFamily, 17, 'bold'), bg='#f4fdfe').place(x=55
 Label(root, text="Receive", font=(fontFamily, 17, 'bold'), bg='#f4fdfe').place(x=290, y=170)
 
 
-background=PhotoImage(file="image/background.jpg")
-Label(root, image=background).place(x=-2, y=323)
+# background=PhotoImage(file="image/background.jpg")
+# Label(root, image=background).place(x=-2, y=323)
 
 
 
